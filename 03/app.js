1859b02946e484f0e6f778f09ff1e5592a06c8e0
Jasmine Ng <jasminee.ngx@gmail.com>
2020-05-24 09:26:00 +0200

    add lesson 3

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,156 @@
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+function setRectangle(gl, x, y, height, width) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+
+  // prettier-ignore
+  var positions = [
+    x1, y1, 
+    x2, y1, 
+    x1, y2, 
+    x1, y2, 
+    x2, y1, 
+    x2, y2
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+}
+
 (function () {
-  var canvas = document.createElement("canvas");
-  var sandbox = new GlslCanvas(canvas);
-  canvas.style.width = "100%";
-  canvas.style.height = "100%";
+  // Get A WebGL context
+  var canvas = document.getElementById("canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+
+  // Get the strings for our GLSL shaders
+  var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+  var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+  var color = [0.2, 0.5, 1.0, 1.0];
+
+  var height = 30;
+  var width = 100;
+  // create GLSL shaders, upload the GLSL source, compile the shaders
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource
+  );
+
+  var translation = [0, 0];
+
+  webglLessonsUI.setupSlider("#x", {
+    slide: updatePosition(0),
+    max: gl.canvas.width - width,
+  });
+  webglLessonsUI.setupSlider("#y", {
+    slide: updatePosition(1),
+    max: gl.canvas.height - height,
+  });
+
+  // Link the two shaders into a program
+  var program = createProgram(gl, vertexShader, fragmentShader);
+
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+
+  // lookup uniforms
+  var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  var colorLocation = gl.getUniformLocation(program, "u_color");
+
+  // Create a buffer and put three 2d clip space points in it
+  var positionBuffer = gl.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // var positions = [0, 0, 30, 0, 0, 20, 0, 20, 30, 0, 30, 20];
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  setRectangle(gl, 0, 0, 30, 100);
+  drawScene();
+
+  // code above this line is initialization code.
+
+  function updatePosition(index) {
+    return function (event, ui) {
+      translation[index] = ui.value;
+      drawScene();
+    };
+  }
+
+  function drawScene() {
+    // code below this line is rendering code.
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    setRectangle(gl, translation[0], translation[1], 30, 100);
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+      positionLocation,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+    gl.uniform4fv(colorLocation, color);
+    // draw
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+  }
 })();
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # webGL-lessons
+
+Ref: https://thebookofshaders.com/
+
+### Install glslViewer
+
+brew update
+brew install glslViewer
+
+### Run glslViewer
+
+glslViewer file_name.frag
+
+### Vertex shaders need data. They can get that data in 3 ways.
+
+Attributes (data pulled from buffers)
+Uniforms (values that stay the same for all vertices of a single draw call)
+Textures (data from pixels/texels)
